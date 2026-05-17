51089364ac194cd43406c3db0a7236cf28513c8c
shamsmo0 <108243138+shamsmo0@users.noreply.github.com>
2026-05-17 19:31:00 +0300

    Delete backend/ITI.ExamSystem.API directory

--- backend/ITI.ExamSystem.API/Models/ChoiceDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ITI.ExamSystem.API.Models
-{
-    public class ChoiceDto
-    {
-        public int ChoiceId { get; set; }
-        public string ChoiceContent { get; set; }
-    }
-}
--- backend/ITI.ExamSystem.API/Models/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ITI.ExamSystem.API.Models
-{
-    public class LoginDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
